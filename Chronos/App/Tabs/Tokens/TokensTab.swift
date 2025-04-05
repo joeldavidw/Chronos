a05e6e5b7430dae59085bece15c84cee85cdc8bb
Joel-David <git@joeldavid.dev>
2025-04-05 16:23:11 +0800

    Fix tag bar visibillity

--- Chronos/App/Tabs/Tokens/TokensTab.swift
+++ Chronos/App/Tabs/Tokens/TokensTab.swift
@@ -129,7 +129,7 @@ struct TokensTab: View {
                                 .padding(.horizontal, 12)
                                 .padding(.vertical, 6)
                                 .foregroundStyle(currentTag == "All" ? .white : (colorScheme == .dark ? .white : .black))
-                                .background(currentTag == "All" ? Color.accentColor : Color(.systemGray4))
+                                .background(currentTag == "All" ? Color.accentColor : Color(.systemFill))
                                 .clipShape(Capsule())
                         }
                         .id("All")
@@ -150,7 +150,7 @@ struct TokensTab: View {
                                     .padding(.horizontal, 12)
                                     .padding(.vertical, 6)
                                     .foregroundStyle(currentTag == tag ? .white : (colorScheme == .dark ? .white : .black))
-                                    .background(currentTag == tag ? Color.accentColor : Color(.systemGray4))
+                                    .background(currentTag == tag ? Color.accentColor : Color(.systemFill))
                                     .clipShape(Capsule())
                             }
                             .id(tag)
